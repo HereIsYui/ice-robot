6acf96b95814e9ad71c992b697a8f50b268f13dc
Yui <44658840+HereIsYui@users.noreply.github.com>
2024-10-19 17:41:19 +0800

    feat(bank): 完善银行功能并添加交易记录存储
    - 存款和取款操作中增加交易中提示信息
    - 生成并保存交易记录到 Redis，保存时间为 7 天
    - 优化交易单号生成逻辑
    - 增加小冰积分的同步更新

--- bot/chats/roles/bank.ts
+++ bot/chats/roles/bank.ts
@@ -1,6 +1,7 @@
 import Fishpi, { ChatData, FingerTo } from "fishpi";
 import config from "../../../config.json";
 import { getUser, updateUser } from "@lib/ice_fun";
+import { setKey } from "@lib/redis";
 
 export default [
   {
@@ -11,13 +12,27 @@ export default [
         await fishpi.chat.send(senderUserName, `金额不合法`);
         return false;
       }
+      await fishpi.chat.send(senderUserName, `交易中...请稍后....`);
       const { userPoint, userNo } = await fishpi.user(senderUserName);
       const user = await getUser(fromId, senderUserName);
       if (userPoint - point >= 0) {
-        let memo = "【IceBank-交易通知】交易单号:" + userNo.toString() + "" + new Date().getTime().toString() + ";PS:交易记录只保存7天";
+        let no = userNo.toString() + new Date().getTime().toString();
+        let memo = "【IceBank-交易通知】交易单号:" + no + ";PS:交易记录只保存7天";
         await FingerTo(config.keys.point).editUserPoints(senderUserName, -point, memo);
+        await FingerTo(config.keys.point).editUserPoints("xiaoIce", point, memo);
         user.point = (user.point ?? 0) + point;
         await updateUser(user);
+        await setKey(
+          `BANK:${no}`,
+          {
+            point: point,
+            uId: user.uId,
+            time: Date.now(),
+            type: 0,
+            memo: memo,
+          },
+          604800
+        );
         await fishpi.chat.send(senderUserName, `成功存 ${point}积分,当前积分:${user.point} \n > ${memo}`);
       } else {
         await fishpi.chat.send(senderUserName, `积分不足`);
@@ -35,13 +50,27 @@ export default [
         await fishpi.chat.send(senderUserName, `金额不合法`);
         return false;
       }
+      await fishpi.chat.send(senderUserName, `交易中...请稍后....`);
       const user = await getUser(fromId, senderUserName);
       const { userNo } = await fishpi.user(senderUserName);
       if (user.point - point >= 0) {
-        let memo = "【IceBank-交易通知】交易单号:" + userNo.toString() + "-" + new Date().getTime().toString() + ";PS:交易记录只保存7天";
+        let no = userNo.toString() + new Date().getTime().toString();
+        let memo = "【IceBank-交易通知】交易单号:" + no + ";PS:交易记录只保存7天";
         await FingerTo(config.keys.point).editUserPoints(senderUserName, point, memo);
+        await FingerTo(config.keys.point).editUserPoints("xiaoIce", -point, memo);
         user.point = (user.point ?? 0) - point;
         await updateUser(user);
+        await setKey(
+          `BANK:${no}`,
+          {
+            point: point,
+            uId: user.uId,
+            time: Date.now(),
+            type: 1,
+            memo: memo,
+          },
+          604800
+        );
         await fishpi.chat.send(senderUserName, `成功取 ${point}积分,当前积分:${user.point} \n > ${memo}`);
       } else {
         await fishpi.chat.send(senderUserName, `积分不足`);
